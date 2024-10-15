14dacdada3cafdee14574b8a21ca4b708b928d86
LadyBetancourt <lady_betancourt24@unikino.edu.mx>
2024-10-15 08:01:24 -0700

    Agregar archivos de proyecto.

---
+++ DiasSemana/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DiasSemana
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dame un numero");
+            string dato = Console.ReadLine();
+
+            int dia = 0;
+            bool sePudoConvertir = false;
+            sePudoConvertir = int.TryParse(dato, out dia);
+            if (sePudoConvertir && dia >= 1 && dia <= 7)
+            {
+                Console.WriteLine(sePudoConvertir + " " + dia);
+            }
+
+            
+        }
+    }
+}
